e2cf59132a4effb8a52ea1cb0b7bae076734c901
Franciele Fernandes <francielefernandes126@gmail.com>
2023-02-10 16:54:46 -0300

    🦄 refactor: Correção das evoluções

--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,7 @@
 
         <h3>Types</h3>
         <ul>
-          <li v-for="type in pokeInfo.types" :key="type.type.name">
-            {{ type.type.name }}
-          </li>
+          <li v-for="type in pokeInfo.types" :key="type.type.name">{{ type.type.name }}</li>
         </ul>
 
         <h3>Abilities</h3>
@@ -34,7 +32,7 @@
           </li>
         </ul>
 
-        
+
         <h3>Stats</h3>
         <ul>
           <li v-for="stat in pokeInfo.stats" :key="stat.stat.name">
@@ -42,135 +40,32 @@
           </li>
         </ul>
 
-       
 
-
+<div v-if="evolucoes">
         <h3>Evoluções</h3>
         <ul>
-          <li v-for="evolution in evolutionChain" :key="evolution.species.name">
-            {{ evolution.species.name }}
+          <li>
+            {{ evolucoes.name }}
+
+            
+            <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evolucoes.url.split('/')[6]}.png`" />
+
           </li>
+
 
           
         </ul>
+
+</div>
+       
+
+
+
+
       </div>
     </div>
 
   </div>
-
-
-  <!-- <v-app>
-    <h1>Ola povo lindo</h1>
-    <v-container>
-      <v-card>
-        <v-container>
-          <v-text-field v-model="search" label="Buscar" placeholder="Digite o seu Pokemon"></v-text-field>
-
-          <v-row>
-            <v-col cols="3" v-for="pokemon in filtrarPokemons" :key="pokemon.name">
-              <v-card @click="show_pokemon(get_id(pokemon))">
-                <v-container>
-                  <v-row class="mx-0 d-flex justify-center">
-                    <img
-                      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(pokemon)}.png`"
-                      :alt="pokemon.name" width="80%">
-                  </v-row>
-
-
-                  <h2 class="text-center">{{ get_name(pokemon) }}</h2>
-                </v-container>
-
-              </v-card>
-
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-dialog v-model="abrir_dialog" width="800">
-
-          <v-card v-if="seleciona_pokemon">
-            <v-container>
-              <v-row class="d-flex align-center">
-                <v-col cols="4">
-                  <img
-                    :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${seleciona_pokemon.id}.png`"
-                    :alt="seleciona_pokemon.name" width="80%">
-                </v-col>
-                <v-col cols="8">
-                  <h1>{{ get_name(seleciona_pokemon) }}</h1>
-                  <ul class="d-flex pl-0">
-                    <li class="d-flex ml-1" v-for="tipo in seleciona_pokemon.types" :key="tipo.type.name">
-
-                      <v-chip label>
-                        {{ tipo.type.name }}
-
-                      </v-chip>
-                    </li>
-
-                  </ul>
-                  <v-divider></v-divider>
-                  <v-chip>Altura: {{ seleciona_pokemon.height * 2.54 }} cm</v-chip>
-                  <v-chip>Peso: {{(seleciona_pokemon.weight / 2.205).toFixed(0) }} Kg</v-chip>
-
-
-
-                  <h2>Moves</h2>
-                  <v-simple-table>
-                    <template >
-                      <thead>
-                        <tr>
-                          <th class="text-left">
-                            Level
-                          </th>
-                          <th class="text-left">
-                            Name
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr v-for="item in filtrar_moves(seleciona_pokemon)" :key="item.move.name">
-                          <td>0</td>
-                          <td>{{ item.move.name }}</td>
-                        </tr>
-                      </tbody>
-                    </template>
-                  </v-simple-table>
-
-
-                  <h2></h2>
-                  <h2>Experiencia: {{ seleciona_pokemon.base_experience }}</h2>
-
-                  <h2>Habilidades</h2>
-                  <ul>
-                    <li v-for="habilidade in seleciona_pokemon.abilities" :key="habilidade.ability.name">
-                      {{ habilidade.ability.name }}
-                    </li>
-                  </ul>
-
-
-
-
-
-                </v-col>
-              </v-row>
-              
-
-            </v-container>
-
-
-
-          </v-card>
-        </v-dialog>
-
-
-
-
-      </v-card>
-    </v-container>
-
-
-  </v-app> -->
-
-
 
 </template>
 
@@ -225,6 +120,7 @@ export default {
       pokeName: '',
       pokeInfo: {},
       evolutionChain: [],
+      evolucoes: {},
     }
   },
 
@@ -242,50 +138,77 @@ export default {
         .then(async response => {
           this.pokeInfo = response.data;
 
-          const evolutionChainResponse = await axios.get(this.pokemon.species.url);
-          this.evolutionChain = evolutionChainResponse.data.evolution_chain.chain;
+          // Verifica se a propriedade species existe
+          if (this.pokeInfo.species) {
+            // Faz a requisição para buscar a Evolution Chain do pokémon
+            axios.get(this.pokeInfo.species.url)
+              .then(response => {
+
+                // Armazena as informações da Evolution Chain em uma variável
+                this.evolutionChain = response.data.evolution_chain.url;
 
 
 
+
+
+                // Verifica se a propriedade evolution_chain existe
+                if (this.evolutionChain) {
+                  // Faz a requisição para buscar as informações da Evolution Chain
+                  axios.get(this.evolutionChain)
+                    .then(response => {
+                      // Armazena as informações da Evolution Chain em uma variável
+                     
+                      this.evolucoes = response.data.chain.evolves_to[0].species;
+                      console.log(typeof this.evolucoes);
+                      
+                    })
+
+
+
+
+                }
+
+              })
+          }
         })
-        .catch(error => {
-          console.log(error);
-        });
-    },
+              .catch(error => {
+                console.log(error);
+              });
+          },
 
 
 
-    get_id(pokemon) {
-      return Number(pokemon.url.split('/')[6])
-    },
-    get_name(pokemon) {
-      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-    },
-    show_pokemon(id) {
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((resposta) => {
-        this.seleciona_pokemon = resposta.data
-        this.abrir_dialog = !this.abrir_dialog
-      })
-    },
-    filtrar_moves(pokemon) {
-      return pokemon.moves.filter(item => {
-        let incluir = false;
-        for (let version of item.version_group_details) {
-          if (version.version_group == "sword-shield") {
-            incluir = true;
+          get_id(pokemon) {
+            return Number(pokemon.url.split('/')[6])
+          },
+          get_name(pokemon) {
+            return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+          },
+          show_pokemon(id) {
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((resposta) => {
+              this.seleciona_pokemon = resposta.data
+              this.abrir_dialog = !this.abrir_dialog
+            })
+          },
+          filtrar_moves(pokemon) {
+            return pokemon.moves.filter(item => {
+              let incluir = false;
+              for (let version of item.version_group_details) {
+                if (version.version_group == "sword-shield") {
+                  incluir = true;
+                }
+              }
+              return incluir;
+            })
+          }
+        },
+          computed: {
+          filtrarPokemons() {
+            return this.pokemons.filter(item => {
+              return item.name.toLowerCase().includes(this.search.toLowerCase())
+            })
           }
         }
-        return incluir;
-      })
-    }
-  },
-  computed: {
-    filtrarPokemons() {
-      return this.pokemons.filter(item => {
-        return item.name.toLowerCase().includes(this.search.toLowerCase())
-      })
-    }
-  }
 
 
 };
