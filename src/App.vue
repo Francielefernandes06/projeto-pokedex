ad991ac6aa267c49228571e2dc339c0402df9633
Franciele Fernandes <francielefernandes126@gmail.com>
2023-02-10 23:28:17 -0300

    🌈 style: estilização (incompleto)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
-<template>
-
-  <div class="container">
-    <div class="search-container ">
+<template  v-if="pokeInfo.types">
 
 
+  <div class="container" >
+   
 
-      <input type="text" v-model="pokeName" />
-      <button @click="submitSearch">Buscar</button>
 
+    <div class="search-container" >
+      <input type="text"  v-model="pokeName">
+      
+     
+      <button class="pesquisar" @click="submitSearch">Buscar</button>
 
     </div>
 
@@ -19,7 +21,7 @@
       <div class="subgrid">
         <div class="emoji"><img src=""></div>
         <div class="type" v-if="pokeInfo.types">
-          <v-chip :color="color" label class="mr-5 white--text" v-for="type in pokeInfo.types" :key="type.type.name">
+          <v-chip  label class="mr-5 white--text" v-for="type in pokeInfo.types" :key="type.type.name" :style="{ backgroundColor: getBackgroundColorByType(type.type.name)}">
             {{ type.type.name }}
           </v-chip>
 
@@ -31,59 +33,43 @@
           <div class="row"><span>Weight</span> <span>{{ pokeInfo.weight }}</span></div>
           <div class="row"><span>Base Experience:</span> <span>{{ pokeInfo.base_experience }}</span></div>
         </div>
-        <div class="picture"><img :src="pokeInfo.sprites.front_default" /></div>
+        <div class="img-pokemon"><img :src="pokeInfo.sprites.front_default" /></div>
       </div>
 
-      <div class="stats">
-        <div class="title" >Stats</div>
-        <div class="graphics" v-for="stat in pokeInfo.stats" :key="stat.stat.name">
-          <div class="row">
-            <div class="name">{{ stat.stat.name }}</div>
-            <div class="bar">
-              <div class="inside" style="width: 20%"></div>
+
+      <div class="dados">
+
+        <div class="stats">
+          <div class="title">Stats</div>
+          <div class="graphics" v-for="stat in pokeInfo.stats" :key="stat.stat.name">
+            <div class="row">
+              <div class="name">{{ stat.stat.name }}</div>
+              <div class="bar">
+                <div class="inside" style="width: 20%"></div>
+              </div>
+              <div class="base"> {{ stat.base_stat }}</div>
             </div>
-            <div class="base"> {{ stat.base_stat }}</div>
-          </div>          
-          
+
+          </div>
         </div>
-      </div>
 
 
+        <div class="evolucoes">
+          <div class="row" v-if="evolucoes">
+            <div class="title">Evolução</div>
 
+            <div class="name">{{ evolucoes.name }}</div>
 
-
-
-
-      <!-- <h3>Abilities</h3>
-          <ul>
-            <li v-for="ability in pokeInfo.abilities" :key="ability.ability.name">
-              {{ ability.ability.name }}
-            </li>
-          </ul>
-
-
-          <h3>Stats</h3>
-          <ul>
-            <li v-for="stat in pokeInfo.stats" :key="stat.stat.name">
-              {{ stat.stat.name }}: {{ stat.base_stat }}
-            </li>
-          </ul>
-
-
-          <div v-if="evolucoes">
-            <h3>Evoluções</h3>
-
-            <p>{{ evolucoes.name }}</p>
             <img
-              :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evolucoes.url.split('/')[6]}.png`" /> -->
+              :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evolucoes.url.split('/')[6]}.png`" />
+          </div>
 
 
+        </div>
 
+      </div>
 
     </div>
-
-
-
 
 
   </div>
@@ -93,37 +79,18 @@
 </template>
 
 <style>
-.container {
+.header{
+  
+  background-color: #DEF3FD;
   display: flex;
-  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  width: 100%;
-  /* height: 100vh;
-  background-image: url('https://www.pockettactics.com/wp-content/sites/pockettactics/2023/01/Pok%C3%A9mon-wallpapers-5.jpg');
-  background-size: cover;*/
-  background-color: rgba(0, 0, 0, 0.5);
-  height: 100vh;
-  width: 100%;
-
-
+  font-size: 30px;
+  font-weight: bold;
+  color: #000;
+  text-transform: uppercase;
+  margin-bottom: 20px;
 }
-
-.search-container {
-  display: flex;
-  align-items: center;
-
-  flex-direction: column;
-  padding: 20px;
-}
-
-input[type="text"] {
-  padding: 10px;
-  font-size: 16px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  margin-bottom: 10px;
-}
-
 
 </style>
 
@@ -147,15 +114,13 @@ export default {
 
   data() {
     return {
-      // pokemons: [],
-      // search: '',
-      // abrir_dialog: false,
-      // seleciona_pokemon: null,
+      color: 'primary',
       searchTerm: '',
       pokeName: '',
       pokeInfo: {},
       evolutionChain: [],
       evolucoes: {},
+      pokemonType: 'water',
     }
   },
 
@@ -208,9 +173,49 @@ export default {
         });
     },
 
+    getBackgroundColorByType(type) {
+      switch (type) {
+         case "grass":
+          return "#33a165";
+        case "poison":
+          return "#7e0058";
+        case "water":
+          return "#0050ac";
+        case "bug":
+          return "#6e1a00";
+        case "ground":
+          return "##c90086";
+        case "fairy":
+          return "#d31c81";
+        case "fighting":
+          return "#634136";
+        case "fire":
+          return "red";
+        case "flying":
+          return "#5d4e75";
+        case "psychic":
+          return "#c90086";
+        case "electric":
+          return "#bba909";
+        case "rock":
+          return "#6e1a00";
+        case "ice":
+          return "#70deff";
+        case "dragon":
+          return "#00c431";
+        case "normal":
+          return "grey";
+        case "ghost":
+          return "#5d4e75";
+        default:
+          return "grey";
+      }
+    }
+
 
   },
   computed: {
+
 
 
 
